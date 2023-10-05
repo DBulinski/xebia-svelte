7edc119c60e5dcb1b0cb76bf6e3f817b3c82ad61
Dawid Bulinski <dawid.bulinski@beamery.com>
2023-10-05 15:29:17 +0200

    Initial Commit

---
+++ src/routes/items/+page.svelte
@@ -0,0 +1,27 @@
+<script>
+    const items = new Array(5000).fill('');
+    let selectedItem = null;
+</script>
+
+{#if selectedItem}
+    <strong>Last item selected: {selectedItem + 1}</strong>
+{/if}
+<section class="items-container">
+    {#each items as _, index}
+        <div class="item">
+            <button disabled={selectedItem === index} on:click={() => selectedItem = index}>Item {index + 1}</button>
+        </div>
+    {/each}
+</section>
+
+<style>
+    .items-container {
+        display: flex;
+        gap: 12px;
+        flex-wrap: wrap;
+    }
+
+    .item {
+        flex: 1;
+    }
+</style>
